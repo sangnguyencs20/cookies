95bbe977612a7636f245e4eaf57cd9b4f84bbc7e
haitrancs <tranhai.tt49@gmail.com>
2023-04-02 00:25:13 +0700

    Update register.php

--- admin/authen/register.php
+++ admin/authen/register.php
@@ -80,52 +80,6 @@ session_start();
     </style>
 </head>
 
-<!-- <body>
-    <div class="container">
-        <div class="panel panel-primary">
-            <div class="panel-heading">
-                <h2 class="text-center">Đăng kí tài khoản</h2>
-                <h4 style="color:red" class="text-center"><?php echo $msg; ?></h4>
-            </div>
-            <form method="POST" onsubmit="return validateForm()" action="">
-                <div class="panel-body">
-                    <div class="form-group">
-                        <label for="usr">Họ và tên</label>
-                        <input name="fullname" required="true" type="text" class="form-control" id="usr">
-                    </div>
-                    <div class="form-group">
-                        <label for="email">Email:</label>
-                        <input name="email" required="true" type="email" class="form-control" id="email">
-                    </div>
-                    <div class="form-group">
-                        <label for="pwd">Password:</label>
-                        <input name="password" minlength="6" required="true" type="password" class="form-control"
-                            id="pwd">
-                    </div>
-                    <div class="form-group">
-                        <label for="confirmation_pwd">Confirmation Password:</label>
-                        <input required="true" type="password" class="form-control" id="confirmation_pwd">
-                    </div>
-                    <p><a href="login.php">Tôi đã có tài khoản</a></p>
-                    <button type="submit" class="btn btn-success">Đăng ký</button>
-                </div>
-
-            </form>
-        </div>
-    </div>
-    <script>
-    function validateForm() {
-        let pwd = document.querySelector("#pwd").value;
-        let confirmPwd = document.querySelector("#confirmation_pwd").value;
-        if (pwd != confirmPwd) {
-            alert("Xác minh mật khẩu không đúng , vui lòng nhập lại")
-            return false
-        }
-        return true;
-    }
-    </script>
-</body> -->
-
 <body>
     <section class="Form">
         <div class="container">
