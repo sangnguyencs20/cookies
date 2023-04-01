e5bf3e7b2e2825f155ff3b0fe08bb6b9e9ea3610
haitrancs <tranhai.tt49@gmail.com>
2023-04-01 21:07:23 +0700

    Indicate cookies database

--- Database/dataset.sql
+++ Database/dataset.sql
@@ -7,6 +7,9 @@
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
+-- Indicating database:
+USE cookies;
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
