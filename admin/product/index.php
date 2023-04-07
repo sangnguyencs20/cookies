4156322fec07201921716752472956196dca47fa
SmallChX <93471226+SmallChX@users.noreply.github.com>
2023-04-07 13:47:34 +0700

    Add files via upload

--- admin/product/index.php
+++ admin/product/index.php
@@ -7,17 +7,17 @@ $baseUrl = '../';
 ?>
 <div class="row" style="margin-top:20px;">
     <div class="col-md-12">
-        <h3 class="text-warning mt-2">Quản lý sản phẩm</h3>
-        <a href="editor.php"><button class="btn btn-success mb-2">Thêm sản phẩm</button></a>
+        <h3 class="mt-2">Quản lý sản phẩm</h3>
+        <a href="editor.php"><button class="btn btn-success mb-2" style="float:right;">Thêm sản phẩm</button></a>
     </div>
-    <table class="table table-bordered table-hover table-responsive">
+    <table class="table table-hover category-table">
         <thead>
             <tr>
-                <th>STT</th>
-                <th>Thumbnail</th>
-                <th>Tên sản phẩm</th>
-                <th>Giá</th>
-                <th>Danh mục</th>
+                <th style="text-align:center;">STT</th>
+                <th style="text-align:center;">Thumbnail</th>
+                <th style="text-align:center;">Tên sản phẩm</th>
+                <th style="text-align:center;">Giá</th>
+                <th style="text-align:center;">Danh mục</th>
                 <th style="width: 50px;"></th>
                 <th style="width: 50px;"></th>
             </tr>
@@ -27,19 +27,19 @@ $baseUrl = '../';
             $index = 0 ;
             foreach($data as $item) {
                 echo '<tr>
-                            <th>'.(++$index).'</th>
-                            <td><img src = "'.fixUrl($item['thumbnail']).'" style="height: 100px;" /></td>
-                            <td>'.$item['title'].'</td>
-                            <td>'.number_format($item['discount']).' vnd'.'</td>
-                            <td>'.$item['category_name'].'</td>
+                            <th style="vertical-align: middle; text-align:center;">'.(++$index).'</th>
+                            <td style="max-width: 200px;"><img src = "'.fixUrl($item['thumbnail']).'" style="max-width: 100%; border-radius:5%;" /></td>
+                            <td style="vertical-align: middle; text-align:center; font-size:18px; font-weight:400;">'.$item['title'].'</td>
+                            <td style="vertical-align: middle; text-align:center; font-size:18px; font-weight:400;">'.number_format($item['discount']).' VND'.'</td>
+                            <td style="vertical-align: middle; text-align:center; font-size:16px; font-weight:300;">'.$item['category_name'].'</td>
 
-                            <td style="width: 50px">
-                                <a href="editor.php?id='.$item['id'].'"><button class="btn btn-warning">Sửa</button></a>
+                            <td style="vertical-align: middle; text-align:center; font-size:14px;">
+                                <a href="editor.php?id='.$item['id'].'"><button class="btn btn-warning" >Sửa</button></a>
                             </td>
-                            <td style="width: 50px">
+                            <td style="vertical-align: middle; text-align:center;">
                             <button onclick="deleteProduct('.$item['id'].')" class="btn btn-danger">Xoá</button>
-                        </td>
-                        </tr>';
+                            </td>
+                    </tr>';
             }
             ?>
         </tbody>
