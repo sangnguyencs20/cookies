644d2dbe0e449461935d6b7f03840d62b3b9bc8d
svn <81409431+sangnguyencs20@users.noreply.github.com>
2023-04-19 11:00:01 +0700

    hot fix

--- Database/config.php
+++ Database/config.php
@@ -3,4 +3,3 @@ define('HOST','localhost');
 define('DATABASE','cookies');
 define('USERNAME','root');
 define('PASSWORD','');
-define('PRIVATE_KEY','asdasf214');
--- admin/authen/login.php
+++ admin/authen/login.php
@@ -26,6 +26,7 @@ if ($user != null) {
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    
     <link rel="stylesheet" href="../../asset/css/authen_register.css">
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
